78b964c60ec74f7e75c970f3aa8bb2f8ec60dcb7
kiwa <kiwa@fresnou.org>
2023-02-06 10:00:47 -0800

    inital commit

---
+++ datatype.js
@@ -0,0 +1,34 @@
+console.log(69);
+//this is a comment
+
+//Numbers
+//Integers
+console.log(-3);
+console.log(0);
+console.log(3);
+//Floating Point Number
+console.log(3.14);
+console.log(100.045);
+//strings
+//Characters, letters, symbol, spaces
+console.log("Hello world");
+console.log("Hello world!!");
+console.log("Hello world 123");
+console.log("'16.0");
+
+//arithmetic operator (math)
+console.log(14+2);
+console.log();
+console.log();
+console.log();
+console.log(14&12);
+console.log(15&10);
+
+//Concatenation: combine strings
+console.log("My name"+"is globe");
+console.log('My' + ' ' + 'age' + ' '+'is'+ ' '+ 15 + '.');
+console.log("Your age is \n14.");
+
+//Temlate literal - plug in a calculated value
+//use backticks '' next to the 1
+console.log('15 divided by 10 = ${15/10} 'axcvzxcvx');
---
+++ operator.js
@@ -0,0 +1,30 @@
+//unary operators use only one value
+console.log(typeof 15);
+console.log(typeof "this string");
+console.log(typeof true);
+console.log(typeof false);
+
+//binary operators use two values
+// comparison operators
+console.log(10 > 5);  //greater than
+
+console.log(10 < 5); //less than
+
+console.log(10 >= 5); //greater than or equal to
+
+console.log(10 <= 5); //less than or equal to
+
+console.log(10 == 5); //are they both equal? false
+
+console.log(10 == 10); //are they both equal? true
+
+console.log(10 != 5); //are they different? true
+
+console.log(10 != 10); //are they different? false
+//ternary operators use three values
+// Logical Operators
+
+console.log(typeof 15);
+console.log(typeof 'this');
+console.log(20 <= 15);
+console.log(17+2);
